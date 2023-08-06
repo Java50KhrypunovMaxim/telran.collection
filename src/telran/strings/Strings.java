2ba70b739a69eb4e1648e777700a49236385b1d4
Maxim Khrypunov <khrypunovmax@gmail>
2023-08-06 16:38:04 +0300

    HW-21

--- src/telran/strings/Strings.java
+++ src/telran/strings/Strings.java
@@ -22,5 +22,30 @@ static public String ipV4Address()
 	String octet = ipV4Octet();
 	return String.format("(%s)(\\.(%s)){3}", octet, octet);
 }
+static public String arithmeticExpression()
+{
+	String operator = operator();
+	String operand = operand();
+	return String.format("\\s*(%s)*(%s%s)*\\s*",operand, operator, operand);
+}
+public static String operator()
+{
+	return "\\s*[-+*/]\\s*";
+}
+public static String operand()
+{
+	String number = anyNumber ();
+	String variable = variableName ();
+	return String.format("(%s|%s)", number, variable);
+}
+
+public static String anyNumber ()
+{
+	return "(\\d+\\.?\\d*|\\.\\d+)";
+}
+public static String  variableName() {
+	
+	return "([a-zA-Z$][a-zA-Z$]*|_[_]*[\\w$]+)";
+}
 
 }
